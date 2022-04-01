2eb2501d00979a0450339a64d76c6e55b586ceae
majoalba20 <mjalba458@gmail.com>
2022-04-01 11:01:22 -0500

    Adding tailwind congig

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="bg-red-500 mt-11">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
